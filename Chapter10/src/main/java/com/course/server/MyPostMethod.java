23debe91d9995638ae97ce65f99022777bee897d
leaflower <c@civilnet.cn>
2018-04-26 23:08:02 +0800

    update chapter10

--- Chapter10/src/main/java/com/course/server/MyPostMethod.java
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -1,19 +1,18 @@
 package com.course.server;
 
 
+import com.course.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @Api(value = "/",description = "这是我全部的post请求")
-@RequestMapping("/V1")
+@RequestMapping("/v1")
 public class MyPostMethod {
 
     //这个变量是用来装我们的cookies信息的
@@ -30,6 +29,30 @@ public class MyPostMethod {
             return "Congratuations to login!";
         }
         return "your userName or password is wrong!";
+    }
 
+    @RequestMapping(value = "/getUserList",method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表",httpMethod = "POST")
+    public String getUserList(HttpServletRequest request,
+                              @RequestBody User u){
+        User user;
+        // /获取cookies
+        Cookie[] cookies=request.getCookies();
+        //验证cookies是否合法
+        for (Cookie c:cookies){
+            if (c.getName().equals("login")
+                    && c.getValue().equals("true")
+                    && u.getUserName().equals("lily")
+                    && u.getPassword().equals("123456")){
+                user=new User();
+                user.setName("Lucy");
+                user.setAge("18");
+                user.setSex("woman");
+                return user.toString();
+
+            }
+        }
+
+        return "参数不合法";
     }
 }
